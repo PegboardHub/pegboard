ef78be716080a15b54d7617ee8aae469893438c3
Elran Shefer <elran777@gmail.com>
2021-01-09 18:15:48 +0200

    Added lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,11 @@ module.exports = {
     'ecmaVersion': 12,
     'sourceType': 'module',
   },
+  'settings': {
+    'react': {
+      'version': 'detect',
+    },
+  },
   'plugins': [
     'react',
     '@typescript-eslint',
